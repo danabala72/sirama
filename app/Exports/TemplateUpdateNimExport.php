c8609ed5d3436a7e5be5b01bb00a88eb04df23ab
danabala72 <danabala72@gmail.com>
2026-05-21 22:27:53 +0800

    Filter mahasiswa by jurusan

--- app/Exports/TemplateUpdateNimExport.php
+++ app/Exports/TemplateUpdateNimExport.php
@@ -3,8 +3,9 @@
 namespace App\Exports;
 
 use App\Models\Mahasiswa;
+use Illuminate\Support\Facades\Auth;
 use Maatwebsite\Excel\Concerns\FromCollection;
-use Maatwebsite\Excel\Concerns\WithHeadings; 
+use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithMapping;
 
 class TemplateUpdateNimExport implements FromCollection, WithHeadings, WithMapping
@@ -14,7 +15,18 @@ class TemplateUpdateNimExport implements FromCollection, WithHeadings, WithMappi
      */
     public function collection()
     {
-          return Mahasiswa::with('user')->get(); 
+        $user = Auth::user();
+        $jurusanId = $user->jurusan->id ?? null;
+
+        $mahasiswa = Mahasiswa::with('user')
+            ->when($jurusanId, function ($query) use ($jurusanId) {
+                $query->whereHas('user', function ($q) use ($jurusanId) {
+                    $q->where('jurusan_id', $jurusanId);
+                });
+            })
+            ->get();
+
+        return $mahasiswa;
     }
     public function headings(): array
     {
@@ -28,9 +40,9 @@ class TemplateUpdateNimExport implements FromCollection, WithHeadings, WithMappi
     public function map($mahasiswa): array
     {
         return [
-            $mahasiswa->user->username ?? '', 
+            $mahasiswa->user->username ?? '',
             $mahasiswa->name,
-            $mahasiswa->nim, 
+            $mahasiswa->nim,
         ];
     }
 }
